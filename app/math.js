20e441694a85798e2da2f00984b0fcec040fe84a
t1t1 <cha@hanmail2.net>
2017-06-03 07:13:53 +0900

    merged from https://github.com/devsrrk/Math-App-for-Children/commit/98799133695cb10d1eb13e5f6b793b28e8ca1ccd

--- app/math.js
+++ app/math.js
@@ -1,70 +1,78 @@
 var math = {
 
 	/**
-     * 배열 크기
+	 * 배열 크기
 	 */
 	arrayCnt : 10,
 
 	/**
-     *
+	 *
 	 * @returns {number}
 	 */
-	makeNumber: function () {
-        return Math.floor(Math.random() * 10) + 1;
-    },
+	makeNumber: function ( digit ) {
+
+		var num = 1;
+		for( var i = 0; i < digit; i++ ) {
+			num = num * 10;
+		}
+
+		return Math.floor(Math.random() * num) + 1;
+	},
 
 	/**
-     *
+	 *
 	 * @returns {Array}
 	 */
-	makeNumberArray: function (operator) {
+	makeNumberArray: function (operator, digit1, digit2) {
 		var tempArray = [];
 
 		for( var i = 0; i < math.arrayCnt; i++ ) {
 
-			var num1 = math.makeNumber();
-			var num2 = math.makeNumber();
-			if( operator == '÷' ) {
-				for( var i = 0; i < 0; i++ ) {
-					if( num1 < num2 ) {
+			var num1 = math.makeNumber(digit1);
+			var num2 = math.makeNumber(digit2);
+			if( operator === '÷' || operator === '-' ) {
+				while(true) {
+					if( num1 > num2 ) {
 						break;
 					} else {
-						num1 = math.makeNumber();
-						num2 = math.makeNumber();
+						num1 = math.makeNumber(digit1);
+						num2 = math.makeNumber(digit2);
 					}
+
+
 				}
 			}
 
 			tempArray.push({
-                num1        : num1,
-                num2        : num2,
-                answer      : '',
-                correct     : '',
-				remainder   : '',
+				num1        : num1,
+				num2        : num2,
+				answer      : null,
+				correct     : null,
+				remainder   : null,
 			});
 		}
 		return tempArray;
 	},
 
-    marking: function ( numArray, calcType ) {
-	    var numArrayLength = numArray.length;
-        for( var i = 0; i < numArrayLength; i++ ) {
+	marking: function ( numArray, calcType ) {
+		var numArrayLength = numArray.length;
+		for( var i = 0; i < numArrayLength; i++ ) {
 
-            if( calcType == '+') {
-                numArray[i].correct = numArray[i].num1 + numArray[i].num2;
-            }
-            if( calcType == '−') {
-                numArray[i].correct = numArray[i].num1 - numArray[i].num2;
-            }
-            if( calcType == '×') {
-                numArray[i].correct = numArray[i].num1 * numArray[i].num2;
-            }
-            if( calcType == '÷') {
-                numArray[i].correct = numArray[i].num1 / numArray[i].num2;
-                numArray[i].remainder = numArray[i].num1 % numArray[i].num2;
-            }
-        }
-        return numArray;
+			if( calcType == '+') {
+				numArray[i].correct = numArray[i].num1 + numArray[i].num2;
+			}
+			if( calcType == '-') {
+				numArray[i].correct = numArray[i].num1 - numArray[i].num2;
+			}
+			if( calcType == '×') {
+				numArray[i].correct = numArray[i].num1 * numArray[i].num2;
+			}
+			if( calcType == '÷') {
+				numArray[i].correct = parseInt(numArray[i].num1 / numArray[i].num2);
+				numArray[i].remainder = parseInt(numArray[i].num1 % numArray[i].num2);
+			}
+		}
+		return numArray;
 	}
 
 };
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,40 +11,69 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', ['$scope', function($scope) {
     $scope.operator = '+';
-    $scope.numberArray = math.makeNumberArray($scope.operator);
     $scope.showAlertFlag = false;
     $scope.showMarkingBtnFlag = true;
+    $scope.showSetting = false;
+    $scope.isTab1 = true;
+    $scope.isTab2 = false;
+    $scope.isTab3 = false;
+    $scope.isTab4 = false;
+    $scope.numOptions = [
+        { code: 1, text: 1 },
+        { code: 2, text: 2 },
+        { code: 3, text: 3 }
+    ];
+    $scope.num1 = $scope.numOptions[0];
+    $scope.num2 = $scope.numOptions[0];
+    $scope.numberArray = math.makeNumberArray($scope.operator, $scope.num1.code, $scope.num2.code);
 
     $scope.marking = marking;
     $scope.reset = reset;
-    $scope.test = test;
+    $scope.toggleSetting = toggleSetting;
 
-    function test(number) {
-    	var isCorrect = (number.answer === number.correct);
-
-    	return {
-    		'glyphicon-ok-circle': isCorrect,
-		    'glyphicon-remove-circle': !isCorrect
-    	};
-    }
-
-	function marking() {
-		$scope.showAlertFlag = true;
-		$scope.numberArray = math.marking( $scope.numberArray, $scope.operator );
+    function marking() {
+        $scope.showAlertFlag = true;
+        $scope.numberArray = math.marking( $scope.numberArray, $scope.operator );
         $scope.showMarkingBtnFlag = false;
     }
 
-    function reset( operator ) {
+    function reset( operator, tabNo ) {
         $scope.operator = operator;
-        $scope.numberArray = math.makeNumberArray();
+        $scope.numberArray = math.makeNumberArray($scope.operator, $scope.num1.code, $scope.num2.code);
         $scope.showAlertFlag = false;
         $scope.showMarkingBtnFlag = true;
+
+        if( tabNo === 'tab1') {
+            $scope.isTab1 = true;
+            $scope.isTab2 = false;
+            $scope.isTab3 = false;
+            $scope.isTab4 = false;
+        } else if( tabNo === 'tab2') {
+            $scope.isTab1 = false;
+            $scope.isTab2 = true;
+            $scope.isTab3 = false;
+            $scope.isTab4 = false;
+        } else if( tabNo === 'tab3') {
+            $scope.isTab1 = false;
+            $scope.isTab2 = false;
+            $scope.isTab3 = true;
+            $scope.isTab4 = false;
+        } else if( tabNo === 'tab4') {
+            $scope.isTab1 = false;
+            $scope.isTab2 = false;
+            $scope.isTab3 = false;
+            $scope.isTab4 = true;
+        }
+    }
+
+    function toggleSetting() {
+        $scope.showSetting = !$scope.showSetting;
     }
 
 }])
 
-.directive('plusQuestions', function() {
-	return {
-		templateUrl: 'app/view1/question_tpl.html'
-	};
+.directive('printQuestions', function() {
+    return {
+        templateUrl: 'app/view1/question_tpl.html'
+    };
 });
